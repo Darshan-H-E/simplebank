e95549bd2c8dd8848f228fb6b85e8662dcc39073
Darshan-H-E <darshanjhe@gmail.com>
2024-11-12 00:13:18 +0530

    added mock db

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -6,20 +6,26 @@ import (
 	"fmt"
 )
 
-// store provides all funcs to execute db queries and txs
-type Store struct {
+// mock store provides all stub funcs to execute db queries and txs
+type Store interface {
+  Querier 
+  TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error)
+}
+
+// sql store provides all funcs to execute sql queries and txs
+type SQLStore struct {
 	*Queries
 	db *sql.DB
 }
 
-func NewStore(db *sql.DB) *Store {
-	return &Store{
+func NewStore(db *sql.DB) Store {
+	return &SQLStore{
 		db:      db,
 		Queries: New(db),
 	}
 }
 
-func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+func (store *SQLStore) execTx(ctx context.Context, fn func(*Queries) error) error {
 	tx, err := store.db.BeginTx(ctx, nil)
 
 	if err != nil {
@@ -55,7 +61,7 @@ type TransferTxResult struct {
 	ToEntry     Entry    `json:"to_entry"`
 }
 
-func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
 	var result TransferTxResult
 	err := store.execTx(ctx, func(q *Queries) error {
 		var err error
